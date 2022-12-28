7a8318aebeb0759512535342592fe6d155a7ffda
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2022-12-28 21:38:09 +0530

    system.cpp

---
+++ system.cpp
@@ -0,0 +1,160 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+using namespace std;
+int main()
+{
+	int a,i=0;
+	string text,old,password1,password2,pass,name,password0,age,user,word,word1,password;
+	string creds[2],cp[2];
+	cout<<"        Security System       "<<endl;
+	cout<<"______________________________"<<endl<<endl;
+	cout<<"|          1.Register        |"<<endl;
+	cout<<"|          2.Login           |"<<endl;
+	cout<<"|          3.Change Password |"<<endl;
+	cout<<"|          4.End Program     |"<<endl;
+	cout<<"|____________________________|"<<endl<<endl;
+	
+	do{
+		cout<<endl<<endl;
+		cout<<"enter your choice:  ";
+		cin>>a;
+		switch(a)
+		{
+			case 1:
+				{
+					cout<<"_______________________________"<<endl<<endl;
+				   cout<<"|-----------Register------------|"<<endl;
+				   cout<<"|_______________________________|"<<endl<<endl;                              
+				   cout<<"Please enter your username:  ";
+				   cin>>name;
+				   cout<<"Please enter the password:  ";
+				   cin>>password0;
+				   cout<<"Please enter your age:  ";
+				   cin>>age;
+				   ofstream of1;
+				   of1.open("file.txt");
+				   if(of1.is_open())
+				   {
+				   	of1<<name<<"\n";
+				   	of1<<password<<"\n";
+				   	cout<<"Registration Successfull"<<endl;
+				   }
+					break;
+				}
+			case 2:
+				{
+					i=0;
+					cout<<"_______________________________"<<endl<<endl;
+				   cout<<"|-------------Login-------------|"<<endl;
+				   cout<<"|_______________________________|"<<endl<<endl;
+				   
+				    ifstream of2;
+				    of2.open("file.txt");
+				    cout<<"Please enter the username:  ";
+				    cin>>user;
+				    cout<<"Please enter the password:  ";
+				    cin>>pass;
+				    if(of2.is_open())
+				    {
+				    	while(!of2.eof())
+				    	{
+				    		while(getline(of2,text)){
+				    		istringstream iss(text);
+							iss>>word;
+							creds[i]=word;
+							i++;	
+							}
+							
+							if(user==creds[0]&&pass==creds[1])
+							{
+								cout<<"---Log in successfull---";
+								cout<<endl<<endl;
+								
+								cout<<"Details: "<<endl;
+								cout<<"Username: "+name<<endl;
+								cout<<"Password: "+pass<<endl;
+								cout<<"Age: "+age<<endl;
+							}
+							else
+							{
+							cout<<endl<<endl;
+							cout<<"Incorrect Credentials"<<endl;
+							cout<<"|       1.Press 2 to Login                 |"<<endl;
+							cout<<"|       2.Press 3 to change password       |"<<endl;	
+							break;
+							}
+							
+						}
+					}
+				  break;
+				  	
+				}
+				case 3:
+					{
+						 i=0;
+						 
+						 cout<<"--------------Change password---------------"<<endl;
+						 ifstream of0;
+						 of0.open("file.txt");
+						 cout<<"Enter the old password: ";
+						 cin>>old;
+						 if(of0.is_open())
+						 {
+						 	while(of0.eof())
+						 	{
+						 		while(getline(of0,text)){
+						 			istringstream iss(text);
+						 			iss>>word1;
+						 			cp[i]=word1;
+						 			i++;
+								 }
+								 if(old==cp[1])
+								 {
+								 	of0.close();
+								 	ofstream of1;
+								 	if(of1.is_open())
+								 	{
+								 		cout<<"Enter your new password: ";
+								 		cin>>password1;
+								 		cout<<"Enter your password again: ";
+								 		cin>>password1;
+								 		
+								 		if(password1==password2)
+								 		{
+								 			of1<<cp[0]<<"\n";
+								 			of1<<password1;
+								 			cout<<" Password change successfully "<<endl;
+										 }
+										 else
+										 {
+										 	of1<<cp[0]<<"\n";
+										 	of1<<old;
+										 	cout<<"Password do not match"<<endl;
+										 }
+									 }
+								 }
+								 else
+								 {
+								 	cout<<" Please enter a valid Password "<<endl;
+								 	break;
+								 }
+							 }
+						 }
+						break;
+					}
+					case 4:
+						{
+							cout<<"_____________Thankyou!______________";
+							break;
+						}
+						default:
+							
+								cout<<"Enter a valid choice";
+							
+		}
+	}
+	while(a!=4);
+	return 0;
+}
